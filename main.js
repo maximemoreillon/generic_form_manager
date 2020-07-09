3ef4c7c807fd9025df83aee874ad057171ea611f
Maxime Moreillon <m.moreillon@gmail.com>
2020-07-09 00:16:23 +0000

    debugging

--- main.js
+++ main.js
@@ -13,7 +13,11 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.send(`Generic form manager API, Maxime MOREILLON`)
+  res.send(`
+    Generic form manager API, Maxime MOREILLON<br>
+    MongoDB URL: ${process.env.MONGODB_URL}<br>
+    Authentication API URL: ${process.env.AUTHENTICATION_API_URL}<br>
+    `)
 })
 
 app.use('/forms', require('./routes/forms.js'))
